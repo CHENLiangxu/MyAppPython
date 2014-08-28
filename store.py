64b6ae0d4acdde6b4714294d536df073b1dec93c
chenliangxu <chenliangxu68@gmail.com>
2014-08-29 00:04:54 +0200

    add bill
    you can add the items into the bill

--- store.py
+++ store.py
@@ -4,10 +4,36 @@ import webapp2
 import jinja2
 import time
 import setting
+import urllib
 
 from google.appengine.ext import ndb
 from google.appengine.api import users
 from main import Account
+
+def isEditor(user):
+  if user:
+    account = Account.query(Account.username == user.nickname()).get()
+    if account:
+      if account.power == 'editor':
+        return True
+  return False
+
+def buildContent(bill_id):
+  bill_tables = BillTable.query()
+  bill_items = Bill.query(Bill.bill_id==bill_id)
+  total_price = 0
+  content = {}
+  for bill_item in bill_items:
+    item = Item.query(Item.code_id==bill_item.item_id).get()
+    content[bill_item.item_id]={
+      'price': item.price,
+      'number': bill_item.number,
+      'total': item.price*bill_item.number,
+    }
+    total_price += content[bill_item.item_id]['total']
+    content['total_price'] = total_price
+    logging.info(content)
+  return content
 
 class Item(ndb.Model):
   code_id = ndb.StringProperty()
@@ -15,30 +41,32 @@ class Item(ndb.Model):
   name = ndb.StringProperty()
   type_item = ndb.StringProperty(choices=setting.TYPE_ITEM)
 
+class Bill(ndb.Model):
+  bill_id = ndb.StringProperty()
+  item_id = ndb.StringProperty()
+  number = ndb.IntegerProperty()
+
+class BillTable(ndb.Model):
+  bill_id = ndb.StringProperty()
+  date = ndb.DateTimeProperty(auto_now_add=True)
+  total_price = ndb.FloatProperty()
+
 class ItemPage(webapp2.RequestHandler):
   def get(self):
     user = users.get_current_user()
-    is_editor = False
-    if user:
-      account = Account.query(Account.username == user.nickname()).get()
-      if account:
-        if account.power == 'editor':
-          url = users.create_logout_url(self.request.uri)
-          url_linktext = 'Logout'
-          is_editor = True
-          items = Item.query()
-          template_values = {
-              'items': items,
-              'url': url,
-              'url_linktext': url_linktext,
-          }
-          template = setting.JINJA_ENVIRONMENT.get_template('template/item.html')
-          self.response.write(template.render(template_values))
-    if not is_editor:
+    if isEditor(user):
+      items = Item.query()
+      template_values = {
+          'items': items,
+      }
+      template = setting.JINJA_ENVIRONMENT.get_template('template/store_item.html')
+      self.response.write(template.render(template_values))
+    else:
       self.redirect(users.create_login_url(self.request.uri))
 
   def post(self):
     #to do: price reglex
+    #to do: check code_id unique
     new_item = Item(
       code_id=self.request.get('code_id'),
       price=float(self.request.get('price')),
@@ -48,6 +76,66 @@ class ItemPage(webapp2.RequestHandler):
     #input xml for add many items at one time
     self.redirect('/item')
 
+class BillPage(webapp2.RequestHandler):
+  def get(self):
+    user = users.get_current_user()
+    if isEditor(user):
+      template_values = {
+          'bill_tables': BillTable.query(),
+      }
+      template = setting.JINJA_ENVIRONMENT.get_template('template/store_bill_table.html')
+      self.response.write(template.render(template_values))
+    else:
+      self.redirect(users.create_login_url(self.request.uri))
+
+  def post(self):
+    timetamp = str(int(time.time()))
+    new_bill_table = BillTable(
+      bill_id='B'+ timetamp,
+      total_price=0, 
+    )
+    new_bill_table.put()
+    self.redirect("/store_bill_table")
+
+class EditeBill(webapp2.RequestHandler):
+  def get(self):
+    user = users.get_current_user()
+    if isEditor(user):
+      bill_id = self.request.get('bill_id')
+      content = buildContent(bill_id)
+      bill_table = BillTable.query(BillTable.bill_id==bill_id).get()
+      bill_table.total_price = content['total_price']
+      bill_table.put()
+      del content['total_price']
+      items = Item.query()
+      template_values = {
+          'content': content,
+          'bill_id': bill_id,
+          'total_price': bill_table.total_price,
+          'items': items,
+      }
+      template = setting.JINJA_ENVIRONMENT.get_template(
+        'template/store_bill.html' )
+      self.response.write(template.render(template_values))
+    else:
+      self.redirect(users.create_login_url(self.request.uri))
+
+  def post(self):
+    #to do item_id is unique
+    operation = self.request.get('Operation')
+    bill_id = self.request.get('bill_id')
+    if operation == 'add':
+      new_item_bill = Bill(
+        bill_id=bill_id,
+        number=int(self.request.get('number')),
+        item_id=self.request.get('item_id'),
+      )
+    new_item_bill.put()
+    query_params = {'bill_id': bill_id}
+    self.redirect('/store_bill?' + urllib.urlencode(query_params))
+
 application = webapp2.WSGIApplication([
-    ('/item', ItemPage),
+    ('/store_item', ItemPage),
+    ('/store_bill_table', BillPage),
+    ('/store_bill?.*', EditeBill),
 ], debug=True)
